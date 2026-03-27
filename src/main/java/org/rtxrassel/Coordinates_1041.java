51eba59e1902d6a47497ad7470ae5d25a47b93f0
deveploper99 <rtxdeveploper99@gmail.com>
2026-03-27 10:17:04 +0600

    problem solve Coordinates 1041

---
+++ src/main/java/org/rtxrassel/Coordinates_1041.java
@@ -0,0 +1,30 @@
+package org.rtxrassel;
+
+import java.util.Scanner;
+
+public class Coordinates_1041 {
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       float x = input.nextFloat();
+       float y = input.nextFloat();
+
+       if (x==0 && y==0){
+           System.out.println("Origem");
+       } else if (x==0) {
+           System.out.println("Eixo Y");
+       } else if (y==0) {
+           System.out.println("Eixo X");
+       }else {
+           if (x>0 && y>0){
+               System.out.println("Q1");
+           } else if (x<0 && y>0) {
+               System.out.println("Q2");
+           } else if (x<0 && y<0) {
+               System.out.println("Q3");
+           } else if (x>0 && y<0) {
+               System.out.println("Q4");
+           }
+       }
+
+   }
+}
